55c89e9266f6df332e3e7dd96c5b339d55d91fec
margotduek <duekis_94@hotmail.com>
2018-01-11 14:44:46 -0600

    lotera :moneybag:

--- parcial_1/loteria/functions.js
+++ parcial_1/loteria/functions.js
@@ -1,12 +1,39 @@
-function mismo_numero(numero){
+function mismo_numero(numero, iteraciones, dias){
   var random_num = 0;
   var ganaste = 0;
   var probabilidad_ganaste = 0;
-  for(int i = 0; i < 5000 ; i++){
-    random_num = Math.floor(Math.random() * 50000);
-    if(random_num == numero){
-      ganaste =+ 1;
+  for(ii = 0; ii < dias ; ii++){
+    for( i = 0; i < iteraciones ; i++){
+      random_num = Math.floor(Math.random() * iteraciones);
+      if(random_num == numero){
+        ganaste =+ 1;
+      }
     }
   }
-  probabilidad_ganaste = (ganaste/5000) * 100;
+  probabilidad_ganaste = (ganaste/iteraciones) * 100;
+  document.getElementById("mismo").innerHTML = probabilidad_ganaste;
+  document.getElementById("iteraciones").innerHTML = iteraciones;
 }
+
+function diferente_numero(iteraciones, dias){
+  var random_num_1 = 0;
+  var random_num_2 = 0;
+
+  var ganaste = 0;
+  var probabilidad_ganaste = 0;
+  for(ii = 0; ii < dias ; ii++){
+    for( i = 0; i < iteraciones ; i++){
+      random_num_1 = Math.floor(Math.random() * iteraciones);
+      random_num_2 = Math.floor(Math.random() * iteraciones);
+
+      if(random_num_1 == random_num_2){
+        ganaste =+ 1;
+      }
+    }
+  }
+  probabilidad_ganaste = (ganaste/iteraciones) * 100;
+  document.getElementById("diferente").innerHTML = probabilidad_ganaste;
+  document.getElementById("iteraciones2").innerHTML = iteraciones;
+}
+mismo_numero(375, 50000, 365);
+diferente_numero(50000, 365);
