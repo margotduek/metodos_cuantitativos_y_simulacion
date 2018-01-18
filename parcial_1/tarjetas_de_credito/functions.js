092fff8c92a799a4e7401e76034390a99cedfcaf
margotduek <duekis_94@hotmail.com>
2018-01-18 14:14:41 -0600

    ahora si listo

--- parcial_1/tarjetas_de_credito/functions.js
+++ parcial_1/tarjetas_de_credito/functions.js
@@ -3,6 +3,13 @@ function pobre_vendedor(llamadas){
   var rand_general = 0;
   var mujeres = 0;
   var hombres = 0;
+  var mujer5 = 0;
+  var mujer10 = 0;
+  var mujer15 = 0;
+  var hombre5 = 0;
+  var hombre10 = 0;
+  var hombre15 = 0;
+  var hombre20 = 0;
 
   for(var i  = 0; i < llamadas; i++){
     rand_general = Math.random() * 1;
@@ -17,12 +24,16 @@ function pobre_vendedor(llamadas){
         if (rand_general <= .25) {
           rand_general = Math.random() * 1;
           if(rand_general <= .1){ // Credito de 5000
+            hombre5++;
             comision += 200;
           }else if (rand_general > .1 && rand_general <= .5) { // Credito de 10000
+            hombre10++;
             comision += 400;
           }else if (rand_general > .5 && rand_general <= .8) { // Credito de 15000
+            hombre15++;
             comision += 600;
           }else if (rand_general > .8 && rand_general <= 1) { // Credito de 20000
+            hombre20++;
             comision += 800;
           }
         }
@@ -33,10 +44,13 @@ function pobre_vendedor(llamadas){
         if (rand_general <= .15) {
           rand_general = Math.random() * 1;
           if(rand_general <= .6){ // Credito de 5000
+            mujer5++;
             comision += 200;
           }else if (rand_general > .6 && rand_general <= .9) { // Credito de 10000
+            mujer10++;
             comision += 400;
           }else if (rand_general > .9 && rand_general <= 1) { // Credito de 15000
+            mujer15++;
             comision += 600;
           }
         }
@@ -46,6 +60,14 @@ function pobre_vendedor(llamadas){
   document.getElementById("comision").innerHTML = comision;
   document.getElementById("hombres").innerHTML = hombres;
   document.getElementById("mujeres").innerHTML = mujeres;
+  document.getElementById("mujer10").innerHTML = mujer10;
+  document.getElementById("mujer15").innerHTML = mujer15;
+  document.getElementById("mujer5").innerHTML = mujer5;
+  document.getElementById("hombre5").innerHTML = hombre5;
+  document.getElementById("hombre10").innerHTML = hombre10;
+  document.getElementById("hombre15").innerHTML = hombre15;
+  document.getElementById("hombre20").innerHTML = hombre20;
+
 
 
 }
