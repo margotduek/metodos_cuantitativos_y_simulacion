ad9e70211667ed3514e505813ad5d469d14ac2a4
margotduek <duekis_94@hotmail.com>
2018-02-12 08:06:01 -0600

    tarea

---
+++ parcial_1/demanda_diaria/functions.js
@@ -0,0 +1,16 @@
+function tarea5(dias){
+  var dist_binom = 0.3125;
+  // Costo de mantener unidad en el inventario es de $1 por dia
+  // Costo del faltante $10 por unidad
+  // Costo de ordenar es $50 por orden
+  var politica_1 = 0;
+  var politica_2 = 0;
+  var articulos_en_stock_1 = 0;
+  var articulos_en_stock_2 = 0;
+
+  for(var i  = 0; i < dias ; i++){
+    if(i%8 == 0 && articulos_en_stock_1 < 30){
+
+    }
+  }
+}
